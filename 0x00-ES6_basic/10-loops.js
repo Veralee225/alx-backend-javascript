efab7a66855871c65e3aa583a20e6cf1f18b3fba
Veralee225 <Veralee225@gmail.com>
2023-01-15 01:08:00 +0100

    fix: fixed lint error

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const returnArray = [];
-    for (const element of array) {
-      returnArray.push(appendString + element);
-    }
-  
-    return returnArray;
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
-  
+
+  return newArray;
+}
