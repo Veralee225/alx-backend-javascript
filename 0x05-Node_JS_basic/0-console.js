5deaa3a540d51a17effffd3f77b5d86602d6c365
Veralee225 <Veralee225@gmail.com>
2023-02-22 03:19:56 +0100

    feat: task 0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,7 @@
+/*This function takes a string argument 'message' 
+and uses console.log method to print out the STDOUT*/
+function displayMessage(message) {
+    console.log(message);
+}
+
+module.exports = displayMessage
