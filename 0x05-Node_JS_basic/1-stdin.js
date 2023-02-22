bd9cc7998a3d64112d897d792e52328f3ef042b1
Veralee225 <Veralee225@gmail.com>
2023-02-22 03:49:28 +0100

    feat: task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,20 @@
+/*A program that takes a stdin */
+
+const readline= require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+console.log('Welcome to Holberton School, what is your name?');
+rl.on('line', (input) =>{
+    console.log(`Your name is: ${input}`);
+    rl.close();
+});
+
+rl.on('close', () => {
+    console.log('\nThis important software is now closing');
+});
+
+module.exports = readline;
