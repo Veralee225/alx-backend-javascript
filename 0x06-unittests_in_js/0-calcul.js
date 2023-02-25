461db615b24b2e7f39924862bd812c47b029c438
Veralee225 <Veralee225@gmail.com>
2023-02-25 03:52:29 +0100

    feat: task 0

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,12 @@
+function calculateNumber(a, b) {
+    const num1 = Number(a);
+    const num2 = Number(b);
+
+    if (Number.isNaN(num1) || Number.isNaN(num2)) {
+        throw TypeError;
+    }
+    const sum = Math.round(num1) + Math.round(num2);
+    return sum;
+}
+
+module.exports = calculateNumber;
