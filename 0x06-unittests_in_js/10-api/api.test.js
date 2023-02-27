96923cb283a7f3eda947c21ab8bb410267e34a08
Veralee225 <Veralee225@gmail.com>
2023-02-27 01:59:50 +0100

    test: api test

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -62,5 +62,48 @@ describe('test the API', () => {
     done();
   });
 
+  it('test the API /login with some body', (done) => {
+    const prm = {
+      url: 'http://localhost:7865/login',
+      method: 'POST',
+      json: {
+        userName: 'Holberton',
+      },
+    };
+    request(prm, (er, rs, bd) => {
+      if (er) throw er;
+      expect(rs.statusCode).to.equal(200);
+      expect(bd).to.equal('Welcome Holberton');
+    });
+    done();
+  });
+
+  it('test the API /login with no body', (done) => {
+    const prm = {
+      url: 'http://localhost:7865/login',
+      method: 'POST',
+      
+    };
+    request(prm, (er, rs, bd) => {
+      if (er) throw er;
+      expect(rs.statusCode).to.equal(200);
+      expect(bd).to.equal('Welcome undefined');
+    });
+    done();
+  });
+
+  it('test the API /available_payments', (done) => {
+    const prm = {
+      url: 'http://localhost:7865/available_payments',
+      method: 'GET',
+    };
+    request(prm, (er, rs, bd) => {
+      if (er) throw er;
+      expect(rs.statusCode).to.equal(200);
+      expect(bd).to.equal('{"payment_methods":{"credit_cards":true,"paypal":false}}');
+    });
+    done();
+  });
+
 
 });
